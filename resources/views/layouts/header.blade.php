446b5709dde8898a388c79f2f37b50b4d69617d8
irfanpertrio <pertrio8@gmail.com>
2022-10-24 17:26:46 +0700

    (fix): edit style

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,25 @@
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top ">
+  <div class="container d-flex align-items-center justify-content-center">
+    <!-- <h1 class="logo"><a href="/">TrashClean</a></h1> -->
+    <!-- Uncomment below if you prefer to use an image logo -->
+    <a href="index.html" class="logo rounded"><img src="assets/img/logo_clean.png" alt="" class="img-fluid"></a>
+
+    <nav id="navbar" class="navbar_page">
+      <ul>
+        <li><a class="nav-link scrollto" href="/">Beranda</a></li>
+        <li><a class="nav-link scrollto" href="/tentang">Tentang</a></li>
+        </li>
+        <li><a class="nav-link scrollto" href="/katalog">Katalog</a></li>
+        <li class="dropdown"><a href="#"><span>Blog Informasi</span> <i class="bi bi-chevron-down"></i></a>
+          <ul>
+            <li><a href="#">Berita</a></li>
+            <li><a href="#">Tutorial</a></li>
+          </ul>
+        </li>
+      </ul>
+      <i class="bi bi-list mobile-nav-toggle"></i>
+    </nav><!-- .navbar -->
+
+  </div>
+</header><!-- End Header -->
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/katalog', function () {
     return view('katalog.index');
 });
+
+Route::get('/tentang', function () {
+    return view('tentang');
+});
