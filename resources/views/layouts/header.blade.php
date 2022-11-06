fa948c3f86dbf04e825d934be456eecea81f960a
irfanpertrio <pertrio8@gmail.com>
2022-11-06 19:16:15 +0700

    (refactor): change admin dashboard content and session for header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,30 +3,36 @@
     <div class="container d-flex align-items-center justify-content-center">
         <!-- <h1 class="logo"><a href="/">TrashClean</a></h1> -->
         <!-- Uncomment below if you prefer to use an image logo -->
-        <a href="index.html" class="logo rounded"><img src={{ url('assets/img/logo_clean.png') }} alt=""
-                class="img-fluid"></a>
+        <a href="index.html" class="logo rounded"><img src={{ url('assets/img/logo_clean.png') }} alt="" class="img-fluid"></a>
 
         <nav id="navbar" class="navbar_page">
             <ul>
                 <li><a class="nav-link scrollto" href="/">Beranda</a></li>
-                <li><a class="nav-link scrollto {{ request()->is('tentang') ? 'active' : '' }}"
-                        href="/tentang">Tentang</a>
+                <li><a class="nav-link scrollto {{ request()->is('tentang') ? 'active' : '' }}" href="/tentang">Tentang</a>
                 </li>
-                <li><a class="nav-link scrollto {{ request()->is('katalog') ? 'active' : '' }}"
-                        href="/katalog">Katalog</a>
+                <li><a class="nav-link scrollto {{ request()->is('katalog') ? 'active' : '' }}" href="/katalog">Katalog</a>
                 </li>
                 <li><a class="nav-link scrollto {{ request()->is('kontak') ? 'active' : '' }}" href="/kontak">Kontak</a>
                 </li>
-                <li class="dropdown"><a href="#"
-                        class="{{ request()->is('tutorial*') ? 'active' : '' }}"><span>Blog
+                <li class="dropdown"><a href="#" class="{{ request()->is('tutorial*') ? 'active' : '' }}"><span>Blog
                             Informasi</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
                         <li><a href="#">Berita</a></li>
                         <li><a href="/tutorial">Tutorial</a></li>
                     </ul>
                 </li>
-                <li><a class="nav-link scrollto {{ request()->is('login') ? 'active' : '' }}" href="/login">Login</a>
                 </li>
+                @if (Auth::check())
+                <li class="dropdown"><a href="#"><span>{{ Auth::user()->username }}</span>
+                        <i class="bi bi-person-circle text-white"></i> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a class="nav-link scrollto" href="/dashboard">Dashboard</a></li>
+                        <li><a href="/logout">Logout</a></li>
+                    </ul>
+                </li>
+                @else
+                <li><a class="nav-link scrollto" href="/login">Login</a></li>
+                @endif
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
