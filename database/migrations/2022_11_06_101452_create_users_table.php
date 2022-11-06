3d2e4dfe976b903301edb9d2fab08094c58de893
irfanpertrio <pertrio8@gmail.com>
2022-11-06 17:20:44 +0700

    (fix): add seeder to table user

--- database/migrations/2022_11_06_101452_create_users_table.php
+++ database/migrations/2022_11_06_101452_create_users_table.php
@@ -14,13 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            // $table->string('name');
-            $table->string('username')->unique();
-            // $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->uuid('id');
             $table->string('role');
-            $table->rememberToken();
+            $table->string('nama', 255);
+            $table->string('no_telepon', 13);
+            $table->string('alamat', 255);
+            $table->string('username');
+            $table->string('password');
             $table->timestamps();
         });
     }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,9 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class UserSeeder extends Seeder
+class UsersSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -15,10 +16,35 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'username' => 'admin',
-            'password' => Hash::make('admin'),
-            'role' => 'admin',
+        DB::table('users')->truncate();     //for cleaning earlier data to avoid duplicate entries
+        DB::table('users')->insert([
+            'id'            => '02b8ca90-5d89-11ed-9b6a-0242ac120001',
+            'nama'          => 'admin',
+            'role'          => 'admin',
+            'no_telepon'    => '081322868886',
+            'alamat'        => 'Anywhere ST. 12345',
+            'username'      => 'admin',
+            'password'      => Hash::make('admin'),
+        ]);
+
+        DB::table('users')->insert([
+            'id'            => '02b8ca90-5d89-11ed-9b6a-0242ac120002',
+            'nama'          => 'Bang Agil',
+            'role'          => 'buyer',
+            'no_telepon'    => '081322868886',
+            'alamat'        => 'Anywhere ST. 12345',
+            'username'      => 'buyer',
+            'password'      => Hash::make('trashclean.id'),
+        ]);
+
+        DB::table('users')->insert([
+            'id'            => '02b8ca90-5d89-11ed-9b6a-0242ac120003',
+            'nama'          => 'Bang Agil',
+            'role'          => 'supplier',
+            'no_telepon'    => '081322868886',
+            'alamat'        => 'Anywhere ST. 12345',
+            'username'      => 'supplier',
+            'password'      => Hash::make('trashclean.id'),
         ]);
     }
 }
